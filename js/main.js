d92c90354efa10aa38654f2284e39c686c539ba1
marloikhodi <deivid.serafim03@gmail.com>
2025-03-01 23:38:20 -0300

    feat: funçao transferir saldo

--- js/main.js
+++ js/main.js
@@ -17,21 +17,36 @@ function bankSystem() {
     var saldoInicial = Number(0)
 
     function verificarSaldo() {
-        let mensagem = `${horarioAtual()}, o seu saldo atual é de R$${saldoInicial}.`
+        const mensagem = `${horarioAtual()}, o seu saldo atual é de R$${saldoInicial}.`
         return console.log(mensagem)
     }
 
     function depositarSaldo() {
         const saldoDepositado = Number(prompt("Digite o saldo a ser depositado: R$"))
         saldoInicial += saldoDepositado
-        let mensagem = `
+        const mensagem = `
             Seu deposito de R$${saldoDepositado} foi feito com sucesso!
             Novo saldo = R$${saldoInicial}.
             `
         return console.log(mensagem)
     }
 
-    verificarSaldo()
+    function transferirSaldo() {
+        const saldoTransferido = Number(prompt("Digite o saldo a ser transferido: R$"))
+        if (saldoTransferido > saldoInicial) {
+            return console.log(`Erro na operação, o valor de R$${saldoTransferido} é menor que o saldo em conta.`)
+        }
+        else {
+            saldoInicial -= saldoTransferido
+            const mensagem = `
+            Seu deposito de R$${saldoTransferido} foi feito com sucesso!
+            Novo saldo = R$${saldoInicial}.
+            `
+            return console.log(mensagem)
+        }
+    }
+
+    transferirSaldo()
 
     // const novaAcao = Number(prompt("Deseja realizar outra ação?"))
     // novaAcao === 1 ? depositarSaldo() : null
