b55ce9dc2387aaf6128b96a5e455e1e386b2a26d
marloikhodi <deivid.serafim03@gmail.com>
2025-03-01 23:23:44 -0300

    feat: funçao depositar e verificar saldo

--- js/main.js
+++ js/main.js
@@ -1,2 +1,40 @@
 const prompt = require('prompt-sync')();
 
+function horarioAtual() {
+    const hora = new Date().getHours()
+
+    const mensagem = {
+        manha: "Bom dia",
+        tarde: "Boa tarde",
+        noite: "Boa noite"
+    }
+
+    return hora < 12 ? mensagem.manha : hora < 18 ? mensagem.tarde : mensagem.noite
+}
+
+function bankSystem() {
+
+    var saldoInicial = Number(0)
+
+    function verificarSaldo() {
+        let mensagem = `${horarioAtual()}, o seu saldo atual é de R$${saldoInicial}.`
+        return console.log(mensagem)
+    }
+
+    function depositarSaldo() {
+        const saldoDepositado = Number(prompt("Digite o saldo a ser depositado: R$"))
+        saldoInicial += saldoDepositado
+        let mensagem = `
+            Seu deposito de R$${saldoDepositado} foi feito com sucesso!
+            Novo saldo = R$${saldoInicial}.
+            `
+        return console.log(mensagem)
+    }
+
+    verificarSaldo()
+
+    // const novaAcao = Number(prompt("Deseja realizar outra ação?"))
+    // novaAcao === 1 ? depositarSaldo() : null
+}
+
+bankSystem()
